8e304c8075d2601447c223c414f1bbd95c856ef1
AVKydin <tlalok@ukr.net>
2022-10-19 16:52:45 +0300

    start

---
+++ main.js
@@ -0,0 +1,25 @@
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext('2d');
+
+// canvas.onmousemove = function (event) {
+    
+//     let x = onclick(offsetX)
+    
+
+//     let y = this.onclick = function (event) {
+//       return event.offsetY
+//     };   
+//     console.log(x, y);
+//     canvas.onmousedown = function (event) {
+
+//     context.lineTo(x, y);
+//     // context.moveTo(x, y);
+//     context.strokeStyle = "red";
+//     context.stroke();
+        
+        
+// }
+    
+ 
+// }
+
